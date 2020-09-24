86ab158db2bc69f3bbe9bea6fa949d614658764c
JosuexD <josuemagallan@gmail.com>
2020-09-24 13:38:30 -0400

    - Init commit

---
+++ Source/prototyping/gui/GUIWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPING_API UGUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/prototyping/prototypingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "prototypingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPING_API AprototypingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
